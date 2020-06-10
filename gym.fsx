d0b16a782766f2b39d35869f2e1e36ea94274689
Dominik.Kotecki <dominikus1910@gmail.com>
2020-06-10 22:20:00 +0200

    add csv read

--- gym.fsx
+++ gym.fsx
@@ -1,5 +1,21 @@
 #r "nuget: FSharp.Plotly"
+#r "nuget: FSharp.Data"
+
 open FSharp.Plotly
+open FSharp.Data
+
+let readCsv (path: string) =
+    try
+        let data = FSharp.Data.CsvFile.Load path
+        data |> Some
+    with ex -> 
+        printfn "%A" ex
+        None
+
 let values =[66; 34]
 let labels =["How much I like this!";"Same but in orange"]
-Chart.Pie(values,labels) |> Chart.Show
+let csv = readCsv "E:\personal_projects\my-gym-statistics\GymRun.csv"
+
+//Chart.Pie(values,labels) |> Chart.Show
+
+printfn "%A" csv
